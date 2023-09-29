1e6c73f66a256f168749dec8ab0b6f630203501b
ananas211 <nickolasgavasheli@gmail.com>
2023-09-29 04:13:27 -0700

    second commit

---
+++ Day2/My Age after 5 Years.py
@@ -0,0 +1,27 @@
+print("this is goa day 2")
+
+#define variebles for curent family memeber ages 
+
+my_age=12
+MoM=35
+DaD=39
+
+#print current ages 
+
+print(f"myage:{my_age}")
+print(f"MoM:{MoM}")
+print(f"DaD:{DaD}")
+
+
+#calculate ages after 5 years
+
+My_age_after_5_years=my_age+5
+MoM_age_after_5_years=MoM+5
+Dad_age_after_5_years=DaD+5
+
+
+#print after 5 years
+
+print(f"my age after 5 years:{My_age_after_5_years}")
+print(f"MoM age after 5 years:{MoM_age_after_5_years}")
+print(f"DaD age after 5 years:{Dad_age_after_5_years}")
