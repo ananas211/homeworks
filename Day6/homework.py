e737648f4445bf5d1055f251383e77ba6d57ebc1
ananas211 <nickolasgavasheli@gmail.com>
2023-11-03 07:44:53 -0700

    sixth commit

---
+++ Day6/homework.py
@@ -0,0 +1,13 @@
+#here is x (aka unknown) and we have to calculate numbers:
+x=[42,8,7,1,0,124,8897,555,3,67,99]
+
+#sum of numbers
+sum_of_numbers=0
+
+for numbers in x:
+    sum_of_numbers=numbers
+#print the answer
+print('sum of numbers=sum_of_numbers')
+
+
+
